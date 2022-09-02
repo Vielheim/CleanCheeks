7ea770ccb55d440c0c9c6e4177276777f749db7e
Terence Ho <>
2022-09-02 20:15:08 +0800

    Add Express Server to backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello!");
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 8000; // Can replace with input from .env file
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+    console.log(`Backend server is listening on Port ${port}`);
+});
