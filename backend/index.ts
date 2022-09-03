22cfc499628c103d8ce6fb097478ce651c80f954
Terence Ho <>
2022-09-03 23:20:35 +0800

    Add body-parser to backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,12 @@
 import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
 
 const app: Express = express();
 const port: number = 8000; // Can replace with input from .env file
+
+// body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
