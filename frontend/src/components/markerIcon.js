37393242582dbe7ff4c3dc8b61eccc1c0568a519
peirong.wu <wupeirong294@gmail.com>
2022-09-14 13:59:59 +0800

    Fix markerIcon offsets

--- frontend/src/components/markerIcon.js
+++ frontend/src/components/markerIcon.js
@@ -3,6 +3,7 @@ import L from 'leaflet';
 import './markerIcon.scss';
 
 const getSvgCircle = (numSections, numGreen) => {
+  console.log(numGreen)
   const RADIUS = 4;
   const PI = 3.14;
   const DIAMETER = 2 * PI * RADIUS;
@@ -17,7 +18,7 @@ const getSvgCircle = (numSections, numGreen) => {
       </defs>
       <use xlink:href="#circle" stroke="pink" stroke-dasharray="${redLength}, ${greenLength}" transform="rotate(270, 5, 5)" />
       <use xlink:href="#circle" stroke="green"
-        stroke-dasharray="0, ${greenLength}, ${redLength}, 0" transform="rotate(270, 5, 5)" />
+        stroke-dasharray="0, ${redLength}, ${greenLength}, 0" transform="rotate(270, 5, 5)" />
       <text x="50%" y="50%" dominant-baseline="central" text-anchor="middle" fill="black" font-size="5">${numSections}</text>
     </svg>
   `;
@@ -25,10 +26,10 @@ const getSvgCircle = (numSections, numGreen) => {
   return svgCircle;
 };
 
-const markerIcon = L.divIcon({
-  html: getSvgCircle(7, 2),
+const getMarkerIcon = (numSections, numGreen) => L.divIcon({
+  html: getSvgCircle(numSections, numGreen),
   iconSize: [40, 40],
   className: "leaflet-div-icon",
 });
 
-export default markerIcon;
+export default getMarkerIcon;
