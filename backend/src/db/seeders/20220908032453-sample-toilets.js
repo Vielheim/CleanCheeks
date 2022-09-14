d1383c76d28045eaccfb33f08bd3d6ba422b4539
Keane Chan <e0425601@u.nus.edu>
2022-09-14 20:32:45 +0800

    use toilets data

--- backend/src/db/seeders/20220908032453-sample-toilets.js
+++ backend/src/db/seeders/20220908032453-sample-toilets.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface) {
-    const toilets = require('../seed.json');
+    const toilets = require('../../../../data/toilets.json');
     let toiletsArray = [];
     toilets.forEach((t) => {
       toiletsArray.push({
