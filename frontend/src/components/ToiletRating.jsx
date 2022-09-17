02284cd042061c746bd215b29dc681749d738e11
Terence Ho <>
2022-09-17 17:37:18 +0800

    Add components to ToiletRating

--- frontend/src/components/ToiletRating.jsx
+++ frontend/src/components/ToiletRating.jsx
@@ -1,55 +1,80 @@
 import React, { useState, useEffect } from 'react';
-import './ToiletRating.scss';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import { TOILET_RATING } from '../constants';
 
+const ToiletRatingCountdown = ({ nextRatingTime }) => {
+  return (
+    <p className="h6 text-muted">
+      You can vote again in {nextRatingTime} minutes
+    </p>
+  );
+};
+
+const ToiletRatingButtons = ({ rateCleanToilet, rateDirtyToilet }) => {
+  return (
+    <Row>
+      <Col className="text-center">
+        <Button variant="success" onClick={rateCleanToilet}>
+          {'Clean! :D'}
+        </Button>
+      </Col>
+
+      <Col className="text-center">
+        <Button variant="danger" onClick={rateDirtyToilet}>
+          {'Dirty! >:('}
+        </Button>
+      </Col>
+    </Row>
+  );
+};
+
 // TODO: Update styling to be consistent with Toilet Details page
-const ToiletRating = () => {
+const ToiletRating = ({ toilet_id, user_id }) => {
   const [nextRatingTime, setNextRatingTime] = useState(null);
 
+  // TODO: Implement this
   useEffect(() => {
-    // Check if user has rated (storage or something)
+    // Check if user has rated this toilet (maybe saved in local storage)
+    // If rated, extract and update nextRatingTime
     // If not rated, display rating buttons
-    // If rated, display next rating time
-    // Not sure if we can refresh the page at the next-rating-time (countdown?)
+    // Maybe we can refresh the page at the nextRatingTime (countdown?)
   }, []);
 
-  const rateToilet = (rating) => {
-    const type = rating;
-    const user_id = '';
-    const toilet_id = '';
+  // TODO: Implement this
+  const rateToilet = async (rating) => {
+    const data = {
+      toilet_id: toilet_id,
+      user_id: user_id,
+      type: rating,
+    };
 
-    // POST to backend, await response
-    // If success: save the next rating date/time -> determine when we can vote again
+    // POST data to backend, await response
+    // If success: save the next rating date/time in local storage and state
+    setNextRatingTime('NEVER');
     // If failure: notify user
   };
 
-  const rateCleanToilet = () => {
-    rateToilet(TOILET_RATING.CLEAN);
+  const rateCleanToilet = async () => {
+    await rateToilet(TOILET_RATING.CLEAN);
   };
 
-  const rateDirtyToilet = () => {
-    rateToilet(TOILET_RATING.DIRTY);
+  const rateDirtyToilet = async () => {
+    await rateToilet(TOILET_RATING.DIRTY);
   };
 
   return (
     <Container className="my-2">
-      <Row>
-        <Col className="text-center">
-          <Button variant="success" onClick={rateCleanToilet}>
-            Clean!
-          </Button>
-        </Col>
-
-        <Col className="text-center">
-          <Button variant="danger" onClick={rateDirtyToilet}>
-            Dirty!
-          </Button>
-        </Col>
-      </Row>
+      {nextRatingTime ? (
+        <ToiletRatingCountdown nextRatingTime={nextRatingTime} />
+      ) : (
+        <ToiletRatingButtons
+          rateCleanToilet={rateCleanToilet}
+          rateDirtyToilet={rateDirtyToilet}
+        />
+      )}
     </Container>
   );
 };
