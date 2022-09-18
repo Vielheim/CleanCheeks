759eb13ddc25ea4fde28cfb05941f07e4426c1e7
peirong.wu <wupeirong294@gmail.com>
2022-09-18 16:56:08 +0800

    stuff

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
-import FilterModal from './FilterModal';
+import FilterOptions from './FilterOptions';
 
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Collapse from 'react-bootstrap/Collapse';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { BsFilter, BsSearch } from 'react-icons/bs';
-import { throttle } from 'lodash';
-import focused_face from '../assets/focused_face.png';
+import throttle from 'lodash/throttle';
 
 import './SearchBar.scss';
 
@@ -18,26 +18,30 @@ const SearchBar = ({ filters, setFilters, venues }) => {
   let searchTimeoutId = 0;
   const [tempSearch, setTempSearch] = useState('');
   const [isShowList, setIsShowList] = useState(false);
-  const [isShowModal, setIsShowModal] = useState(false);
+  const [isShowFilters, setIsShowFilters] = useState(false);
 
-  const handleModalClose = () => {
-    setIsShowModal(false);
-  };
-
-  const handleModalSubmit = ({ gender, haveShowers }) => {
-    setFilters({
+  const handleModalChange = ({ gender, haveShowers }) => {
+    setFilters((filters) => ({
       ...filters,
       gender,
       haveShowers,
-    });
+    }));
   };
 
   const onSearchChange = ({ target: { value } }) => {
     setTempSearch(value);
   };
 
+  const onFormFocus = () => {
+    if (searchTimeoutId > 0) {
+      clearTimeout(searchTimeoutId);
+    }
+    setIsShowFilters(false);
+    setIsShowList(true);
+  };
+
   const onFormBlur = () => {
-    searchTimeoutId = setTimeout(() => setIsShowList(false), 500);
+    searchTimeoutId = setTimeout(() => setIsShowList(false), 300);
   };
 
   const onListFocus = () => {
@@ -47,10 +51,10 @@ const SearchBar = ({ filters, setFilters, venues }) => {
   };
 
   const onListItemClick = ({ target: { value } }) => {
-    setFilters({
+    setFilters((filters) => ({
       ...filters,
       search: value,
-    });
+    }));
     setTempSearch(value);
     setIsShowList(false);
   };
@@ -65,62 +69,61 @@ const SearchBar = ({ filters, setFilters, venues }) => {
   const thrFilterVenues = throttle(filterVenues, 1000);
 
   return (
-    <>
-      <Container className="filter-row">
-        <Row>
-          <Col xs={10}>
-            <InputGroup>
-              <InputGroup.Text className="p-0">
-                <img src={focused_face} height={30} width={38} />
-              </InputGroup.Text>
-              <Form.Control
-                placeholder="Where are you? Eg: UTown"
-                onChange={onSearchChange}
-                onFocus={() => setIsShowList(true)}
-                onBlur={onFormBlur}
-                value={tempSearch}
+    <Container className="filter-row">
+      <Row>
+        <Col>
+          <InputGroup>
+            <InputGroup.Text>
+              <BsSearch />
+            </InputGroup.Text>
+            <Form.Control
+              className="search-bar"
+              placeholder="Where are you? Eg: UTown"
+              onChange={onSearchChange}
+              onFocus={onFormFocus}
+              onBlur={onFormBlur}
+              value={tempSearch}
+            />
+            <Collapse in={!isShowList} dimension="width">
+              <div>
+                <Button
+                  className="filter-options-button"
+                  onClick={() => setIsShowFilters(!isShowFilters)}
+                >
+                  <BsFilter height={22} />
+                </Button>
+              </div>
+            </Collapse>
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="mt-1">
+          <Collapse in={isShowList}>
+            <ListGroup className="list-group" onFocus={onListFocus}>
+              {thrFilterVenues(venues).map((id) => (
+                <ListGroup.Item
+                  key={id}
+                  action
+                  onClick={onListItemClick}
+                  value={id}
+                >
+                  {`${id} (${venues[id].roomName})`}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Collapse>
+          <Collapse in={isShowFilters}>
+            <div>
+              <FilterOptions
+                handleModalChange={handleModalChange}
+                state={filters}
               />
-            </InputGroup>
-          </Col>
-          <Col
-            className="d-flex justify-content-end filter-modal-button"
-            xs={2}
-          >
-            <Button onClick={() => setIsShowModal(true)}>
-              <BsFilter height={22} />
-            </Button>
-          </Col>
-        </Row>
-        {isShowList && (
-          <Row className="mt-1">
-            <Col>
-              <ListGroup
-                className="list-group"
-                onFocus={onListFocus}
-                onBlur={() => setIsShowList(false)}
-              >
-                {thrFilterVenues(venues).map((id) => (
-                  <ListGroup.Item
-                    key={id}
-                    action
-                    onClick={onListItemClick}
-                    value={id}
-                  >
-                    {`${id} (${venues[id].roomName})`}
-                  </ListGroup.Item>
-                ))}
-              </ListGroup>
-            </Col>
-          </Row>
-        )}
-      </Container>
-      <FilterModal
-        show={isShowModal}
-        handleModalClose={handleModalClose}
-        handleModalSubmit={handleModalSubmit}
-        state={filters}
-      />
-    </>
+            </div>
+          </Collapse>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
