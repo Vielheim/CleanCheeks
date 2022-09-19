ccdaf5ef80dd4d4baa8fb03ef5293c6cdb1095ff
Keane Chan <e0425601@u.nus.edu>
2022-09-19 11:50:24 +0800

    update controller to use userId in localStorage

--- frontend/src/api/ToiletController.js
+++ frontend/src/api/ToiletController.js
@@ -1,7 +1,13 @@
 import Api from './api';
+import { getLocalStorageValue } from '../utilities/localStorage';
+import { USER_ID_KEY } from '../constants';
 
 export default class ToiletControlller {
-  static async fetchCloseToilets(coordinates, radius, userId = 'testuser') {
+  static async fetchCloseToilets(
+    coordinates,
+    radius,
+    userId = getLocalStorageValue(USER_ID_KEY) ?? 'testuser'
+  ) {
     return Api.makeApiRequest({
       method: 'GET',
       url: `/toilets/neighbours?latitude=${coordinates[0]}&longitude=${coordinates[1]}&radius=${radius}&userId=${userId}`,
