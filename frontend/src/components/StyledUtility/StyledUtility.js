044f8c5f616e9551649d86aeebbe9586641dd43b
peirong.wu <wupeirong294@gmail.com>
2022-09-21 17:05:51 +0800

    Fix errors

--- frontend/src/components/StyledUtility/StyledUtility.js
+++ frontend/src/components/StyledUtility/StyledUtility.js
@@ -1,10 +1,6 @@
 import { capitalize } from 'lodash';
 import { GrFormCheckmark, GrFormClose } from 'react-icons/gr';
-<<<<<<< HEAD:frontend/src/components/shared/StyledUtility.jsx
 import styles from './StyledUtility.module.scss';
-=======
-import './StyledUtility.scss';
->>>>>>> main:frontend/src/components/StyledUtility/StyledUtility.js
 
 const StyledUtility = ({ utility, presentUtilities }) => {
   const isPresent = presentUtilities.includes(utility.toUpperCase());
